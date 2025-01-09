58748a6899057ec9e41ff6849b2de37538589255
Kiesha Maria <kiesha.maria@kalvium.community>
2025-01-09 18:14:05 +0530

    mongodb-client completed

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
-const connect_db = require('./Database/database');
+const { getDb, connection } = require('./Database/mongo-client.js');
+const connect_db = require('./Database/database.js');
 if (process.env.NODE_ENV !== 'PRODUCTION') {
     require('dotenv').config();
   }
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const checkStatus = await connection.connect();
+  const readyState = connection.topology.isConnected()
+    ? 'connected'
+    : 'disconnected';
+    res.send(`<h3>Database Connection Status : ${readyState}</h3>`);
+});
 
 
-  app.get('/home', async (req, res) => {
-    const dbStatus = await connect_db();
-    res.send(`Database Connection Status: ${dbStatus}`);
-  });
 
+ 
+  
 const port = 3000;
 app.listen(port, () => {
-    connect_db();
+ 
     console.log(`Server is running on http://localhost:${port}`);
 });
